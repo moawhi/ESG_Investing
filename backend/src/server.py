3d2fb8a3acc1d9ec9119e791d94c97f5175575c5
Minh Duong Nguyen <54211192+moawhi@users.noreply.github.com>
2024-03-13 18:54:51 +1100

    resolve conflict

--- backend/src/server.py
+++ backend/src/server.py
@@ -19,9 +19,6 @@ def register():
     last_name = info["last_name"]
     email = info["email_address"]
     password = info["password"]
-    print(email, password)
-    response_data = auth.auth_register(first_name, last_name,email, password)
-    print("Returning response:", response_data)
 
     try:
         user_id = auth.auth_register(first_name, last_name, email, password)
