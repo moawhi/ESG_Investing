ba608738a8047e4489d2d82a2ea0f72f148e8b5d
Alyssa Tran <z5358355@ad.unsw.edu.au>
2024-03-27 20:46:59 +1100

    Implement list frameworks functionality

---
+++ backend/src/framework.py
@@ -0,0 +1,45 @@
+"""
+Framework functions
+"""
+
+import mysql.connector
+
+def framework_list(token):
+    """
+    Gets all available frameworks in the database.
+    Provides the name and the information of each framework.
+    """
+    db = None
+    try:
+        db = mysql.connector.connect(user="esg", password="esg", host="127.0.0.1", database="esg_management")
+        
+        query = """
+            SELECT name, info
+            FROM framework
+        """
+        frameworks = []
+        with db.cursor() as cur:
+            cur.execute(query)
+            for framework in cur.fetchall():
+                (name, info) = framework
+                framework_details = {
+                    "name": name,
+                    "info": info
+                }
+                frameworks.append(framework_details)
+
+            return {
+                "frameworks": frameworks
+            }
+
+    except Exception as err:
+        print(f"Error: {err}")
+
+    finally:
+        if db.is_connected():
+            db.close()
+
+def framework_default_metrics(token, framework):
+    """
+    Gets the default metrics for a selected framework
+    """
