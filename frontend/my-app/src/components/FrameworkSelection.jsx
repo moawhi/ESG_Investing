8ffbf96feeae518033bac87ba9bfd6d2394f2d94
Lucy Jing <z5360318@ad.unsw.edu.au>
2024-04-07 02:12:57 +1100

    finished making ui for company info

--- frontend/my-app/src/components/FrameworkSelection.jsx
+++ frontend/my-app/src/components/FrameworkSelection.jsx
@@ -1,89 +1,53 @@
 import React, { useState } from 'react';
-import { Modal, Box, RadioGroup, Radio, FormControlLabel, Button, Typography } from '@mui/material';
+import { Box, RadioGroup, Radio, FormControlLabel, Typography } from '@mui/material';
 
-const FrameworkSelection = ({ open, onClose, frameworks, onSelectFramework }) => {
+const FrameworkSelection = ({ frameworks, onSelectFramework }) => {
 
   const [selectedFramework, setSelectedFramework] = useState('');
-  const [showError, setShowError] = useState(false);
 
-  const handleChange = (click) => {
-    setSelectedFramework(click.target.value);
-    setShowError(false);
-  };
-
-  const handleFrameworkSelection = () => {
-    if (selectedFramework) {
-      onSelectFramework(selectedFramework);
-      onClose(); 
-    } else {
-      setShowError(true); 
-    }
+  const handleChange = (event) => {
+    const newFramework = event.target.value;
+    setSelectedFramework(newFramework);;
+    onSelectFramework(newFramework);
   };
 
   return (
-    <Modal
-      open={open}
-      onClose={handleFrameworkSelection}
-    >
-      <Box       
-        sx = {{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'background.paper',
-        borderRadius: '16px',
-        boxShadow: 16,
-        p: 4,
-      }}>
-        <Typography id="select-framework-modal" variant="h6" component="h2">
-          Select a Framework
-        </Typography>
-        <Box id="select-framework-modal" 
-        sx={{
-          mt: 2,
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 0.5, 
-          }}>
-          <RadioGroup
-            name="framework-selection"
-            value={selectedFramework}
-            onChange={handleChange}
-          >
-            {frameworks.map((framework) => (
-              <FormControlLabel
-                key={framework.framework_id}
-                value={framework.framework_id.toString()}
-                control={<Radio />}
-                label={
-                  <Box sx={{ mb: 1 }}>
-                    <Typography variant="body1">{framework.name}</Typography>
-                    <Typography variant="body2" color="textSecondary" sx={{ fontSize: '0.75rem' }}>
-                      {framework.info}
-                    </Typography>
-                  </Box>
-                }
-              />
-            ))}
-          </RadioGroup>
-          {showError && (
-          <Typography color="error" sx={{ mt: 1, fontSize: '0.8rem' }}>
-            Please select a framework.
-          </Typography>
-        )}
-          <Box sx={{ mt: 1 }}>
-            <Button 
-            onClick={handleFrameworkSelection}
-            sx={{
-              color: '#779c73'
-            }}> 
-            Select</Button>
-          </Box>
-        </Box>
+    <Box       
+      sx = {{ pt: 4, pl: 4, pr: 4 }}>
+      <Typography sx={{ fontSize: '1.5rem', fontWeight: 'bold' }}>
+        Select a Framework
+      </Typography>
+      <Box
+      sx={{
+        mt: 2,
+        display: 'flex',
+        flexDirection: 'column',
+        maxHeight: '45vh',
+        overflow: 'auto'
+        }}>
+        <RadioGroup
+          name="framework-selection"
+          value={selectedFramework}
+          onChange={handleChange}
+        >
+          {frameworks.map((framework) => (
+            <FormControlLabel
+              key={framework.framework_id}
+              value={framework.framework_id.toString()}
+              control={<Radio />}
+              label={
+                <Box sx={{ mb: 2 }}>
+                  <Typography variant="body1" sx={{ fontSize: '1.2rem', mb: 0.5 }}>{framework.name}</Typography>
+                  <Typography variant="body2" color="textSecondary">
+                    {framework.info}
+                  </Typography>
+                </Box>
+              }
+            />
+          ))}
+        </RadioGroup>
       </Box>
-    </Modal>
+    </Box>
   );
 };
 
