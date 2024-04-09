26d9eb0309bfcd507c39b9f3b407454300214359
Alyssa Tran <z5358355@ad.unsw.edu.au>
2024-04-10 00:35:06 +1000

    Add company_id to company details return

--- backend/src/company.py
+++ backend/src/company.py
@@ -69,7 +69,7 @@ def get_company_details(company_id):
         with db.cursor(dictionary=True) as cursor:
             # Fetch company details, ESG rating, industry, and industry ranking directly
             cursor.execute("""
-                SELECT name, info, esg_rating, industry, industry_ranking
+                SELECT perm_id as company_id, name, info, esg_rating, industry, industry_ranking
                 FROM company
                 WHERE perm_id = %s
             """, (company_id,))
